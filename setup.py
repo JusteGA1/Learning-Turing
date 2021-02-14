80b3142724495ad5c0f871e8914683b2b8a69e0d
Juste <juste.gaviene@gmail.com>
2021-02-14 15:07:19 +0200

    setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Tesla factory',
+    version='0.0.1',
+    desription='tesla class with functions for car, e.g.lock, charge, etc.',
+    author='Juste Gaviene',
+    author_email='juste.gaviene@gmail.com',
+    url='https://github.com/JusteGA1/Learning-Turing',
+    packages=['factory'],
+    package_dir={'': 'Learning-Turing'},
+)
